ae4b28505e282e79a15708dba603932febd08da7
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2023-09-01 17:16:17 +0100

    add algorithms module

---
+++ algorithms/src/main/java/org/a04t/java_repo/algo/App.java
@@ -0,0 +1,8 @@
+package org.a04t.java_repo.algo;
+
+public class App {
+    public static void main(String[] args) {
+        String banner = "==================================";
+        System.out.println(String.join("\n", banner, "This is Algo app in java-repo", banner));
+    }
+}
