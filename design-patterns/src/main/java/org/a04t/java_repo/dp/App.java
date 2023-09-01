4cc4739bf04d75a7462dbaa05e17c64953797998
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2023-09-01 17:28:09 +0100

    add design-patterns module

---
+++ design-patterns/src/main/java/org/a04t/java_repo/dp/App.java
@@ -0,0 +1,8 @@
+package org.a04t.java_repo.dp;
+
+public class App {
+    public static void main(String[] args) {
+        String banner = "==================================";
+        System.out.println(String.join("\n", banner, "This is Design Patterns app in java-repo", banner));
+    }
+}
